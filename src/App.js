92339417557c051b78f15770b773eaefb983a060
Ksenia <99985774+ksenia-kuksina@users.noreply.github.com>
2023-06-03 19:50:38 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+
+function App() {
+    return (
+
+            <div className="app">
+                        <Checkout />
+                        <Header />
+                        <Home />
+            </div>
+    );
+}
+
+export default App;
