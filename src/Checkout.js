afb5b3aba9558c3516d1425136f3eca9f05345a4
Ksenia <99985774+ksenia-kuksina@users.noreply.github.com>
2023-06-11 01:05:28 +0400

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,29 @@
 import React from "react";
 import "./Checkout.css";
-
+import Subtotal from "./Subtotal";
 
 function Checkout() {
-    return <div className={"checkout"}><p>KEEEFTEEEMEEE</p></div>;
+    return (
+        <div className={"checkout"}>
+            <div className={"checkout__left"}>
+              <img className={"checkout__ad"} src={"https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"}  />
+
+              <div className={"checkout__title"}>
+                  <h2>Your shopping Basket</h2>
+
+                  {/*BasketItem*/}
+                  {/*BasketItem*/}
+                  {/*BasketItem*/}
+                  {/*BasketItem*/}
+                  {/*BasketItem*/}
+              </div>
+            </div>
+
+            <div className={"checkout__right"}>
+                <Subtotal/>
+            </div>
+        </div>
+    );
 }
 
 
