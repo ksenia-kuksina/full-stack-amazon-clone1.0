ab2e117e94d84d4496aefa23a27b9c3cb4f86c55
Ksenia <99985774+ksenia-kuksina@users.noreply.github.com>
2023-06-11 01:02:40 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-
+        <Router>
             <div className="app">
-                        <Checkout />
-                        <Header />
-                        <Home />
+                <Header />
+                <Routes>
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
             </div>
+        </Router>
     );
 }
 
