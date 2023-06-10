efcb8867ae5b29e8c3d8206b31a6c6da73510c1d
Ksenia <99985774+ksenia-kuksina@users.noreply.github.com>
2023-06-11 01:10:35 +0400

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
+function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+    console.log("this is the basket >>>", basket);
+    const addToBasket = () => {
+        // Dispatch an action to add the product to the basket
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
 
-function Product({id, title, image, price, rating}) {
     return (
-        <div className={"product"}>
-            <div className={"product__info"}>
+        <div className="product">
+            <div className="product__info">
                 <p>{title}</p>
-                <p className={"product__price"}>
+                <p className="product__price">
                     <small>$</small>
                     <strong>{price}</strong>
                 </p>
-              <div className={"product__rating"}>
-                  {Array(rating).fill().map((_, i) => (
-                      <p>⭐</p>
-                  ))}
-              </div>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_, i) => (
+                        <p key={i}>⭐</p>
+                    ))}
+                </div>
             </div>
 
-            <img src={image}/>
+            <img src={image} alt={title} />
 
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     );
 }
