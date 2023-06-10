0ba9839509771d5bac5968ccc5b2d30f418acaa3
Ksenia <99985774+ksenia-kuksina@users.noreply.github.com>
2023-06-11 01:08:32 +0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,59 +1,50 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
+import { Link } from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-
+import {useStateValue} from "./StateProvider";
 
 function Header() {
+
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
-        <div className={"header"}>
-            <img  className={"header__logo"}
-                  src={"http://pngimg.com/uploads/amazon/amazon_PNG11.png"} />
+        <div className="header">
+            <Link to="/">
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon Logo" />
+            </Link>
 
-
-            <div className={"header__search"}>
-                <input className={"header__searchInput"} type={"text"}/>
-                <SearchIcon className={"header__searchIcon"} />
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                <SearchIcon className="header__searchIcon" />
             </div>
 
-
-            <div className={"header__nav"}>
-                <div  className={"header__option"}>
-                    <span className={"header__optionLineOne"}>
-                        Hello Guest
-                    </span>
-                    <span className={"header__optionLineTwo"}>
-                        Sign In
-                    </span>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                    <span className="header__optionLineTwo">Sign In</span>
                 </div>
 
-            <div  className={"header__option"}>
-                 <span className={"header__optionLineOne"}>
-                        Returns
-                 </span>
-                <span className={"header__optionLineTwo"}>
-                       & Orders
-                </span>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineTwo">& Orders</span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
             </div>
 
-            <div  className={"header__option"}>
-                <span className={"header__optionLineOne"}>
-                     Your
-                </span>
-                <span className={"header__optionLineTwo"}>
-                     Prime
-                </span>
-            </div>
-            </div>
-
-            <div className={"header__optionBasket"}>
-                <ShoppingBasketIcon />
-                <span className={"header__optionLineTwo header__basketCount"} >0</span>
-            </div>
+            <Link to="/checkout">
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
+                </div>
+            </Link>
         </div>
-)
+    );
 }
 
-
-
-export default Header
+export default Header;
