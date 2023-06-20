d62228349df867cc9b4d2b0b3a97e0f89758895a
Ksenia <99985774+ksenia-kuksina@users.noreply.github.com>
2023-06-21 03:54:15 +0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -21,10 +21,14 @@ function Header() {
             </div>
 
             <div className="header__nav">
-                <div className="header__option">
-                    <span className="header__optionLineOne">Hello Guest</span>
-                    <span className="header__optionLineTwo">Sign In</span>
-                </div>
+                <Link to={"/login"}>
+
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello Guest</span>
+                        <span className="header__optionLineTwo">Sign In</span>
+                    </div>
+
+                </Link>
 
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
@@ -35,7 +39,6 @@ function Header() {
                     <span className="header__optionLineOne">Your</span>
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
-            </div>
 
             <Link to="/checkout">
                 <div className="header__optionBasket">
@@ -44,6 +47,7 @@ function Header() {
                 </div>
             </Link>
         </div>
+      </div>
     );
 }
 
