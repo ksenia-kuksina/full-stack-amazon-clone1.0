6ba8645d2a6324cd8b91ceeb0f4a37b16a48f613
Ksenia <99985774+ksenia-kuksina@users.noreply.github.com>
2023-06-21 03:55:22 +0400

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,10 @@
+export const initialState = {
+    basket: [],
+    user: null
+};
+
+export const getBasketTotal = (basket) =>
+    basket?.reduce((amount, item) => item.price + amount, 0);
 const reducer = (state, action) => {
     console.log(action);
     switch (action.type) {
@@ -15,7 +22,8 @@ const reducer = (state, action) => {
             let newBasket = [...state.basket];
 
             if (index >= 0) {
-                newBasket.splice(index, 1); // Remove the item from the newBasket array
+                newBasket.splice(index, 1);
+
             } else {
                 console.warn(`Can't remove product (id: ${action.id}) as it's not in the basket!`);
             }
